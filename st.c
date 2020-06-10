2c30e4a18fee170fa0cc85dd653e55d9fea0a1fe
CSharpCheddar <muellermartin98@gmail.com>
2020-06-10 15:10:35 -0500

    Added xclearwin patch.

--- st.c
+++ st.c
@@ -1909,10 +1909,8 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid color j=%d, p=%s\n",
 				        j, p ? p : "(null)");
 			} else {
-				/*
-				 * TODO if defaultbg color is changed, borders
-				 * are dirty
-				 */
+				if (j == defaultbg)
+					xclearwin();
 				redraw();
 			}
 			return;
